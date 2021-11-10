82de8ac16f2dd69b1f6005aec1e5df36f937a37e
VeraGD <vgd1003@alu.ubu.es>
2021-11-10 12:29:23 +0100

    Algunos gráficos

--- cargarDatos.R
+++ cargarDatos.R
@@ -10,7 +10,7 @@ actFisica <- read_excel("INPUT/DATA/Act_Fisica.xlsx",
 
 AF <-
 actFisica %>% 
-  slice(2:21) %>% 
+  slice(3:21) %>% 
   rename(Comunidades = ...1, d1_2 = `1 o 2 días a la semana`, 
          d3_4 = `3 o 4 días a la semana`,
          d5_6 = `5 o 6 días a la semana`,
@@ -31,7 +31,7 @@ saludMental <- read_excel("INPUT/DATA/s_mental.xlsx",
                           range = "A7:CS71")
 SM <- saludMental %>% 
   rename(Comunidades = ...1, depresión = ...60, ansiedad = ...63) %>% 
-  slice(c(3:22))
+  slice(c(4:22))
   
 
 # select(c(1,60,63))  
@@ -43,7 +43,9 @@ library(readxl)
 zonasVerdes <- read_excel("INPUT/DATA/satisf_ZV.xlsx", 
                           range = "A7:F27")
 ZV <- zonasVerdes %>% 
+  slice(2:20) %>% 
   rename(comunidades = ...1, Valoración = `Valoración media`)
+  
 
 # select(c(1,6)) 
 
@@ -70,6 +72,22 @@ AF_ZV <-
 AF_ZV 
 View(AF_ZV)
 
+ #ggplot(data = AF_ZV, aes (x  = Comunidades))+
+  #geom_bar(aes(fill = Valoración))
+
+#ggplot(data = AF_ZV, aes(x = Valoración, y = Días)) +
+  #geom_point(aes(colour = factor(Comunidades))) +
+  #geom_smooth()
+
+
+
+AF_ZV %>% 
+  filter(Valoración > 4) %>% 
+  ggplot(data = ., aes(x = Valoración, y = Días)) +
+    geom_point(colour = "red") +
+    geom_smooth() +
+    theme_bw()
+
 #levels(AF_ZV$Comunidades)
 
 # Relación entre actividad física y salud mental.
@@ -83,6 +101,19 @@ AF_SM <-
 
 AF_SM
 View(AF_SM)
+
+AF_SM %>% 
+  ggplot(data = ., aes(x = ansiedad, y = Días)) +
+  geom_point(colour = "red") +
+  geom_smooth() +
+  theme_bw() # no me pinta la linea
+
+
+AF_SM %>% 
+  ggplot(data = ., aes(x = depresión, y = Días)) +
+  geom_point(colour = "red") +
+  geom_smooth() +
+  theme_bw()
 
 # Relación entre zonas verdes y salud mental.
 
