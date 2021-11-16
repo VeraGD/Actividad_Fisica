c84dca771bfef8195677fa008e75a7cc31eb368a
VeraGD <vgd1003@alu.ubu.es>
2021-11-16 18:23:07 +0100

    Actualizacion data

--- cargarDatos.R
+++ cargarDatos.R
@@ -1,12 +1,30 @@
 # Cargar datos ------------------------------------------------------------
 
-# Tabla número de días por semana de ejercicio físico durante el tiempo de ocio según sexo y comunidad autónoma. Población de 15 y más años.
 library(readxl)
 library(dplyr)
 library(tidyverse)
 
+# Tabla número de días por semana de ejercicio físico
 actFisica <- read_excel("INPUT/DATA/Act_Fisica.xlsx", 
                         range = "A7:H70")
+
+# Tabla de enfermedades crónicas
+saludMental <- read_excel("INPUT/DATA/s_mental.xlsx", 
+                          range = "A7:CS71")
+
+# Tabla satisfacción con las zonas verdes
+zonasVerdes <- read_excel("INPUT/DATA/satisf_ZV.xlsx", 
+                          range = "A7:F27")
+
+
+
+# Creación de tablas ------------------------------------------------------
+
+
+# * Actividad física ------------------------------------------------------
+
+# Tabla número de días por semana de ejercicio físico durante el tiempo de ocio 
+# según sexo y comunidad autónoma. Población de 15 y más años.
 
 AF <-
 actFisica %>% 
@@ -21,51 +39,53 @@ actFisica %>%
   summarise(.data = ., dias_prom = mean(Días, na.rm = TRUE))
   
 
-AF$Comunidades <- as.factor(AF$Comunidades)
-
-
+#AF$Comunidades <- as.factor(AF$Comunidades)
+AF
 View(AF)  
 
-# Tabla problemas o enfermedades crónicas o de larga evolución padecidas en los últimos 12 meses y diagnosticadas por un médico según sexo y comunidad autónoma.
 
-library(readxl)
-library(dplyr)
 
-saludMental <- read_excel("INPUT/DATA/s_mental.xlsx", 
-                          range = "A7:CS71")
+# * Salud mental ----------------------------------------------------------
+
+# Tabla problemas o enfermedades crónicas o de larga evolución padecidas en los 
+# últimos 12 meses y diagnosticadas por un médico según sexo y comunidad autónoma.
+
 SM <- saludMental %>% 
   rename(Comunidades = ...1, depresión = ...60, ansiedad = ...63) %>% 
   pivot_longer(names_to = "Enfermedades", values_to = "Personas", cols = c(depresión, ansiedad)) %>%
   slice(c(7:44)) %>% 
   select(c(1,96,97))
   
-
-# select(c(1,60,63))  
 SM
 View(SM)
 
-# Tabla satisfacción con las zonas verdes y áreas recreativas por CCAA  y nivel de satisfacción.
-library(readxl)
-zonasVerdes <- read_excel("INPUT/DATA/satisf_ZV.xlsx", 
-                          range = "A7:F27")
+
+
+# * Satisfacción de zonas verdes ------------------------------------------
+
+# Tabla satisfacción con las zonas verdes y áreas recreativas por CCAA  
+# y nivel de satisfacción.
+
 ZV <- zonasVerdes %>% 
   slice(2:20) %>% 
   rename(comunidades = ...1, Valoración = `Valoración media`)
-  
-
-# select(c(1,6)) 
 
 ZV
 View(ZV)
 
+
 # Comprobar que todos los niveles son iguales.
 
 levels(AF$Comunidades)
-ZV$Comunidades
+ZV$comunidades
 SM$Comunidades
 
 
-# Relación entre zonas verdes y actividad física.
+
+# Objetivos específicos ---------------------------------------------------
+
+
+# * Relación entre zonas verdes y actividad física. -----------------------
 
 AF_ZV <-  
   AF %>% 
@@ -78,25 +98,19 @@ AF_ZV <-
 AF_ZV 
 View(AF_ZV)
 
- #ggplot(data = AF_ZV, aes (x  = Comunidades))+
-  #geom_bar(aes(fill = Valoración))
-
-#ggplot(data = AF_ZV, aes(x = Valoración, y = Días)) +
-  #geom_point(aes(colour = factor(Comunidades))) +
-  #geom_smooth()
-
-
-
+ 
+# Gráfica
 AF_ZV %>% 
   filter(Valoración > 4) %>% 
   ggplot(data = ., aes(x = Valoración, y = dias_prom)) +
-    geom_point(colour = "red") +
+    geom_point(aes(colour = factor(Comunidades))) +
     geom_smooth() +
     theme_bw()
 
-#levels(AF_ZV$Comunidades)
 
-# Relación entre actividad física y salud mental.
+
+# * Relación entre actividad física y salud mental. -----------------------
+
 AF_SM <-  
   AF %>% 
   select(Comunidades, dias_prom) %>% 
@@ -108,15 +122,17 @@ AF_SM <-
 AF_SM
 View(AF_SM)
 
+
+# Gráfica
 AF_SM %>% 
   ggplot(data = ., aes(x = Personas, y = dias_prom)) +
-  geom_point(colour = "red") +
+  geom_point(aes(colour = factor(Comunidades))) +
   geom_smooth() +
   theme_bw()# no me pinta la linea
 
 
-levels(AF$Frecuencia)
-# Relación entre zonas verdes y salud mental.
+
+# * Relación entre zonas verdes y salud mental. ---------------------------
 
 ZV_SM <-  
   ZV %>% 
@@ -129,9 +145,11 @@ ZV_SM <-
 ZV_SM
 View(ZV_SM)
 
+
+# Gráfica
 ZV_SM %>% 
   filter(Valoración > 4) %>% 
-  ggplot(data = ., aes(x = Valoración, y = Personas)) +
-  geom_point(colour = "red") +
+  ggplot(data = ., aes(x = Personas, y = Valoración)) +
+  geom_point(aes(colour = factor(comunidades))) +
   geom_smooth() +
   theme_bw()
